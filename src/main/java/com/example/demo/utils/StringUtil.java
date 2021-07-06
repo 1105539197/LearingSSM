5ced92b988eb9c3a2d53ed119d9d0ba74c7f4331
zhangchen <1105539197@qq.com>
2021-07-06 17:06:47 +0800

    0

---
+++ src/main/java/com/example/demo/utils/StringUtil.java
@@ -0,0 +1,14 @@
+package com.example.demo.utils;
+
+/**
+ * @Classname StringUtil
+ * @Description TODO
+ * @Date 2021/7/6 16:17
+ * @Created by ZhangChen
+ */
+
+public class StringUtil {
+    public static boolean notEmpty(String s) {
+        return s != null && s.length() > 0;
+    }
+}
