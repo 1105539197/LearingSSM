7e3561d3eb3321c847c3661ddadcdafa2a27ec37
zhangchen <1105539197@qq.com>
2021-08-03 15:54:44 +0800

    1

--- src/main/java/com/example/demo/controller/ListController.java
+++ src/main/java/com/example/demo/controller/ListController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Class;
 import com.example.demo.service.ClassService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import java.util.List;
  * @Date 2021/7/6 16:38
  * @Created by ZhangChen
  */
+@Slf4j
 @RestController
 @RequestMapping("/hello")
 public class ListController {
--- src/main/java/com/example/demo/entity/Class.java
+++ src/main/java/com/example/demo/entity/Class.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -27,5 +28,7 @@ public class Class {
 
     @TableField(exist = false)
     private List<Student> students;
+
+    private Date time;
 
 }
